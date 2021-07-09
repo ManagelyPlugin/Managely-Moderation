62131f7434471d0fc255e49ac47172fda421e0b8
skrekhere <liam@tannock.net>
2021-07-09 13:31:24 -0700

    initial

---
+++ src/main/java/org/managely/ManagelyModeration/Main.java
@@ -0,0 +1,10 @@
+package org.managely.ManagelyModeration;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable(){
+
+    }
+}
